2a48d11cc083f992705cc301f5a4f074cae1d4dc
raqueldeang <raqueldeang@gmail.com>
2020-08-22 04:03:50 +0800

    updated code

--- javascripts/modules/theme-module.js
+++ javascripts/modules/theme-module.js
@@ -1,48 +1,21 @@
 $(function() {
   initResponsiveHeader();  
+  collapseAccordion();
+  popupCart();
 
   $(window).on("load", function() {
     initNavbar();
-
-    var heroBanner = new Swiper('.section-banner .swiper-container', {
-      loop: true,
-      slidesPerView: 1,
-      noSwiping: false,
-      autoplay: {
-        delay: 6700,
-      },
-      speed: 900,
-      pagination: {
-        el: '.section-banner .swiper-pagination',
-        clickable: true,
-          renderBullet: function (index, className) {
-            return '<span class="' + className + '">0' + (index + 1) + '</span>';
-          },
-      },
-    });
-
-    var collection = new Swiper('.section-collections .swiper-container', {
-      loop: false,
-      slidesPerView: 2,
-      spaceBetween: 55,
-      autoplay: {
-        delay: 6700,
-      },
-      speed: 900,
-      navigation: {
-        nextEl: '.section-collections .swiper-button-next',
-        prevEl: '.section-collections .swiper-button-prev',
-      },
-    });
+    swiperHomeBanner();
+    swiperCollection();
   });
 
   $(window).on('resize', () => {
     initResponsiveHeader();
-    // initHeaderScrolled();
+    initHeaderScrolled();
   });
 
   $(window).on('scroll', () => {
-    // initHeaderScrolled();
+    initHeaderScrolled();
   });
 
   function initNavbar() {
@@ -92,5 +65,70 @@ $(function() {
     } else {
       header.removeClass('sticky-top');
     }
+  }
+
+  function swiperHomeBanner() {
+    var heroBanner = new Swiper('.section-banner .swiper-container', {
+      loop: true,
+      slidesPerView: 1,
+      noSwiping: false,
+      autoplay: {
+        delay: 6700,
+      },
+      speed: 900,
+      pagination: {
+        el: '.section-banner .swiper-pagination',
+        clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">0' + (index + 1) + '</span>';
+          },
+      },
+    });
+  }
+
+  function swiperCollection() {
+    var collection = new Swiper('.section-collections .swiper-container', {
+      loop: false,
+      slidesPerView: 2,
+      spaceBetween: 55,
+      autoplay: {
+        delay: 6700,
+      },
+      speed: 900,
+      navigation: {
+        nextEl: '.section-collections .swiper-button-next',
+        prevEl: '.section-collections .swiper-button-prev',
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        575: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        }
+      }
+    });
+  }
+
+  function collapseAccordion() {
+    $('.collapse').on('shown.bs.collapse', function(){
+      $(this).parent().find(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
+    }).on('hidden.bs.collapse', function(){
+      $(this).parent().find(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
+    });
+  }
+
+  function popupCart() {
+    $(document).on('click', '.open-cart', function() {
+      $(".popup-cart").addClass("show");
+      $("body").addClass("bg-overlay");
+    });
+  
+    $(document).on('click', '.close-cart', function() {
+      $(".popup-cart").removeClass("show");
+      $("body").removeClass("bg-overlay");
+    });
   }
 });
